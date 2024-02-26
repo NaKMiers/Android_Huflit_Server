0619d46b9210248bd6199f626380654b055af5f3
Pi Pi <diwas118151@gmail.com>
2024-02-26 14:46:20 +0700

    ok

--- src/app/controllers/ImageController.js
+++ src/app/controllers/ImageController.js
@@ -20,7 +20,7 @@ class ImageController {
     }
   }
 
-  // [POST]: /completions/:id/prompt
+  // [POST]: /image/create-prompt
   async createPrompt(req, res) {
     console.log('createPrompt')
 
@@ -89,7 +89,7 @@ class ImageController {
       const images = await openai.images.generate({
         model: 'dall-e-2',
         prompt,
-        n: amount > 10 ? 10 : amount || 1,
+        n: +amount > 10 ? 10 : +amount || 1,
         size: ['256x256', '512x512', '1024x1024'].includes(size) ? size : '256x256',
       })
 
