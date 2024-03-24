dfec133569f9fd3fbbc619663a51574b4f739a3a
Pi Pi <diwas118151@gmail.com>
2024-03-24 22:06:09 +0700

    ok

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -161,7 +161,7 @@ class UserController {
       // checking whether old password is correct
       const isValidPassword = await bcrypt.compare(oldPassword, user.password)
       if (!isValidPassword) {
-        return res.status(404).json({ message: 'Old password is not correct' })
+        return res.status(404).json({ message: 'Mật khẩu cũ không đúng' })
       }
 
       // create new password
@@ -170,7 +170,7 @@ class UserController {
       user.save()
 
       // response message
-      res.status(200).json({ message: 'Changed password successfully!!!' })
+      res.status(200).json({ message: 'Đổi mật khẩu thành công!!!' })
     } catch (err) {
       res.status(500).json({ message: err.message })
     }
